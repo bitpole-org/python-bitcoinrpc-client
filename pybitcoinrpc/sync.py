a3c903893522383ad734bce6abae5a6f07ae780b
Maxim Gurov <psylopunk@protonmail.com>
2020-10-30 18:37:15 +0300

    add connect and getbestblockhash

--- pybitcoinrpc/sync.py
+++ pybitcoinrpc/sync.py
@@ -1,5 +1,22 @@
-
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 class Client:
-    def __init__(self, node=None):
+    def log(self, text): print(f"[Client] {text}")
+
+    def debug(self, text):
+        if self.debug:
+            print(f"[sync.Client ~ debug] {text}")
+
+    def __init__(self, node=None, debug=False):
         self.node_addr = node
+        self.debug = debug
+        self.connected = False
+
+    def connect(self, node=None):
+        self.node_addr = node or self.node_addr
+        self.connected = True
+
+        self.rpc_connection = AuthServiceProxy(f"http://{self.node_addr.replace("http://")}")
+
+    def getbestblockhash(self):
+        if not connected: raise Exception("Connect to bitcoin node")
